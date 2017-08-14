388d7d038c1b4a7c24e173371a8e5dd698617352
Jess <jesesnz@gmail.com>
2017-08-14 14:55:14 +1000

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,25 @@
 console.log("up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 
-console.log("user flipped" +" " + cardOne);
-console.log("user flipped" +" " + cardThree);
+
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+var cardTwo = cards[1];
+//var cardThree = cards[];
+//var cardFour = cards[];
+
+cardsInPlay.push(cardOne);
+console.log("user just flipped" + " " + cardOne);
+cardsInPlay.push(cardTwo);
+console.log("user just flipped" + " " + cardTwo);
+
+if (cardsInPlay.length === 2){
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		alert("you found a match!");
+	}else{
+		alert("sorry try again");
+	}
+}
